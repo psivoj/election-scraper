c03ecffa9abc3022f0c0b5ea47580dd777692a5e
Magdalena Slanska <magdalena@slansti.cz>
2023-09-24 12:43:40 +0200

    Parsing the city

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ discord: magdalena2586
 
 import argparse
 import requests
+import re
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
@@ -29,18 +30,55 @@ def main():
     main_page_request = requests.get(args.url)
     main_page_parsed = BeautifulSoup(main_page_request.text, features="html.parser")
 
-    city_array = main_page_parsed.select("table td.cislo a")
-    for city_a in city_array:
-        city_href = urljoin(args.url, city_a.attrs["href"])
-        parse_city(city_href)
+    results = []
+    parties = {}
+
+    city_rows = main_page_parsed.select("table tr")
+    for city_row in city_rows:
+        code = city_row.select_one(".cislo a")
+        location = city_row.select_one(".overflow_name")
+        if code is None or location is None:
+            continue
+        city_row = {
+            "code": code.text,
+            "location": location.text
+        }
+
+        city_href = urljoin(args.url, code.attrs["href"])
+        city_results = parse_city(city_href)
+        city_row.update(city_results)
+        results.append(city_row)
+        if len(parties.keys()) == 0:
+            print("Extracting parties")
+            parties = city_results["parties"].keys()
+
+
+def select_to_int(html, selector):
+    return int(re.sub(r"\s*", "", html.select_one(selector).text))
+
 
 def parse_city(url):
-    city_results = {}
-    print(url)
+    city_results = {
+        "parties": {}
+    }
+
+    city_request = requests.get(url)
+    city_parsed = BeautifulSoup(city_request.text, features="html.parser")
+
+    city_results["registered"] = select_to_int(city_parsed, 'td[headers="sa2"]')
+    city_results["envelopes"] = select_to_int(city_parsed, 'td[headers="sa3"]')
+    city_results["valid"] = select_to_int(city_parsed, 'td[headers="sa6"]')
+
+    party_results = city_parsed.select("div.t2_470 tr")
+    for party_result in party_results:
+        party = party_result.select_one('td[headers="t1sa1 t1sb2"]')
+        if party is None:
+            continue
+        city_results["parties"][party.text] = select_to_int(party_result, 'td[headers="t1sa2 t1sb3"]')
+
     return city_results
 
 
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
