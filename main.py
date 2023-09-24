5dcea10c9ecad4497b3f179ecb221bf63fc22e64
Magdalena Slanska <magdalena@slansti.cz>
2023-09-24 11:54:30 +0200

    Scraping main page

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ discord: magdalena2586
 """
 
 import argparse
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
 def main():
@@ -22,6 +25,19 @@ def main():
 
     print(f"URL: {args.url}")
     print(f"Outfile: {args.outfile}")
+
+    main_page_request = requests.get(args.url)
+    main_page_parsed = BeautifulSoup(main_page_request.text, features="html.parser")
+
+    city_array = main_page_parsed.select("table td.cislo a")
+    for city_a in city_array:
+        city_href = urljoin(args.url, city_a.attrs["href"])
+        parse_city(city_href)
+
+def parse_city(url):
+    city_results = {}
+    print(url)
+    return city_results
 
 
 if __name__ == '__main__':
